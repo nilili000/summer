391f1f395f45a412a91bfcb81ad8195d8189d4c9
nilili000 <wildtess21c@gmail.com>
2026-08-19 16:01:13 +0900

    mydong update 12:07

--- session_two.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def order():
-    st.write('-------------- session_two.py문서  order() --------------')
-    if 'user_list'  in st.session_state:
-        st.write("test")
-    else:
-        st.write('세션목록에 없습니다')
